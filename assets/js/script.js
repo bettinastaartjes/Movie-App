5deb3451be6c366a52b4e3e0e45cc3e1f43ea9ca
13 Miles <bennygabel@gmail.com>
2022-09-12 14:27:49 -0400

    rename object/elements/names

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,8 @@ const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=14bd8f982f89e1b1a206780215c62b44&query="';
 
-const form = document.getElementById('form')
-const search = document.getElementById('search')
+const formEl = document.getElementById('form')
+const searchEl = document.getElementById('search')
 
 // Get initial movies
 getMovies(API_URL)
