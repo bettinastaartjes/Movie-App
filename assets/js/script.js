c5531a17b365b56d2a8e55340f76e9b3f3f99f7d
13 Miles <bennygabel@gmail.com>
2022-09-13 14:32:45 -0400

    implement search by 4 movie types; by default - 'discoer' or by search - if search criteria was entered

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,8 @@ URL: /discover/movie?with_genres=18&primary_release_year=2014
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=14bd8f982f89e1b1a206780215c62b44&query="';
 
+
+
 /* 
 Other API calls
 ---------------
@@ -40,6 +42,11 @@ https://www.themoviedb.org/documentation/api/discover
 const formEl = document.getElementById('form')
 const mainEl = document.getElementById('main')
 const searchEl = document.getElementById('search')
+
+const movieOptEl = document.getElementById("movie_opt")
+const movieTypeEl = document.getElementById("movie_type")
+
+
 
 // Get initial movies
 getMovies(API_URL)
@@ -110,13 +117,65 @@ formEl.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const searchTerm = search.value
+    const searchType = form.movie_type.value
+    
 
     if(searchTerm && searchTerm !== '') {
-        getMovies(SEARCH_API + searchTerm)
+
+/*
+      pular movies?
+      URL: /discover/movie?sort_by=popularity.desc
+      
+      What are the highest rated movies rated R?
+      URL: /discover/movie/?certification_country=US&certification=R&sort_by=vote_average.desc
+      
+      What are the most popular kids movies?
+      URL: /discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc
+      
+      What is are the best movies from 2010?
+      URL: /discover/movie?primary_release_year=2010&sort_by=vote_average.desc
+      
+      What are the best dramas that were released this year?
+      URL: /discover/movie?with_genres=18&primary_release_year=2014
+      
+*/
+
+        // getMovies(SEARCH_API + searchTerm)
+
+        switch( searchType) {
+          case "Popular":
+            getMovies('https://api.themoviedb.org/3/search/movie?sort_by=popularity.desc&api_key=14bd8f982f89e1b1a206780215c62b44&page=1&query="'+searchTerm+'"') ;
+            break;
+          case "Kids":
+            getMovies('https://api.themoviedb.org/3/search/movie?certification_country=US&certification=R&sort_by=vote_average.desc&api_key=14bd8f982f89e1b1a206780215c62b44&page=1&query="'+searchTerm+'"') ;
+            break;
+          case "Best":
+            getMovies('https://api.themoviedb.org/3/search/movie?primary_release_year=2020&sort_by=vote_average.desc&api_key=14bd8f982f89e1b1a206780215c62b44&page=1&query="'+searchTerm+'"') ;
+            break;
+          case "Drama":
+            getMovies('https://api.themoviedb.org/3/search/movie?with_genres=18&primary_release_year=2014&api_key=14bd8f982f89e1b1a206780215c62b44&page=1&query="'+searchTerm+'"') ;
+            break;
+        }
 
         search.value = ''
-    } else {
-        window.location.reload()
+     } else {
+        //  window.location.reload()
+
+        // If Search criteria is blank 
+        switch( searchType) {
+          case "Popular":
+            getMovies('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=14bd8f982f89e1b1a206780215c62b44&page=1') ;
+            break;
+          case "Kids":
+            getMovies('https://api.themoviedb.org/3/discover/movie?certification_country=US&certification=R&sort_by=vote_average.desc&api_key=14bd8f982f89e1b1a206780215c62b44&page=1') ;
+            break;
+          case "Best":
+            getMovies('https://api.themoviedb.org/3/discover/movie?primary_release_year=2020&sort_by=vote_average.desc&api_key=14bd8f982f89e1b1a206780215c62b44&page=1') ;
+            break;
+          case "Drama":
+            getMovies('https://api.themoviedb.org/3/discover/movie?with_genres=18&primary_release_year=2014&api_key=14bd8f982f89e1b1a206780215c62b44&page=1') ;
+            break;
+        }
+
     }
 })
-
