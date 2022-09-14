b8c26b39577011fcd56b4841e545497cd15a5360
Bettina Staartjes <bbstaars@gmail.com>
2022-09-14 17:02:26 -0400

    added fonts, changed color of the header, changed fonts and colors of texts in divs etc

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 
 /*
 
-pular movies?
+Popular movies?
 URL: /discover/movie?sort_by=popularity.desc
 
 What are the highest rated movies rated R?
